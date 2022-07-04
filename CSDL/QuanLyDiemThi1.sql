8bbbf17cb21808ebb46f2528b1d0db75a8a4fad5
LAPTOP-69EUOU7G\Hp <lmtri9118@gmail.com>
2022-07-04 15:49:56 +0700

    update

---
+++ CSDL/QuanLyDiemThi1.sql
@@ -0,0 +1,6 @@
+SELECT * FROM mark;
+-- insert into student (student_name,address,phone,status,class_id) values ('Hung','Ha Noi','0912113113',1,1);
+-- insert into student (student_name,address,status,class_id) values ('Hoa','Hai Phong',1,1);
+-- insert into student  (student_name,address,phone,status,class_id) values ('Manh','HCM','0123123123',0,2);
+-- insert into subject values (1,'CF',5,1),(2,'C',5,1),(3,'HDJ',6,1),(4,'RDBM',10,1);
+insert into mark (sub_id,student_id,mark,examtimes) values (1,1,8,1),(1,2,10,2),(2,1,12,1);
